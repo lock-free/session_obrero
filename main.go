32f2877286f01ca044fbd4854738f40d5bf810c9
loveKino <842914439@qq.com>
2019-10-15 23:44:14 +0800

    convert to stdserv.StdWorkerConfig

--- main.go
+++ main.go
@@ -4,15 +4,13 @@ package main
 import (
 	"github.com/lock-free/gopcp"
 	"github.com/lock-free/gopcp_stream"
-	"github.com/lock-free/obrero"
 	"github.com/lock-free/obrero/mids"
 	"github.com/lock-free/obrero/napool"
+	"github.com/lock-free/obrero/stdserv"
 	"github.com/lock-free/obrero/utils"
 	"github.com/lock-free/session_obrero/session"
 	"time"
 )
-
-const APP_CONFIG = "/data/app.json"
 
 var pcpClient = gopcp.PcpClient{}
 
@@ -24,19 +22,8 @@ type AppConfig struct {
 
 func main() {
 	var appConfig AppConfig
-	err := utils.ReadJson(APP_CONFIG, &appConfig)
-	if err != nil {
-		panic(err)
-	}
-
-	var naPools napool.NAPools
-
-	naPools = obrero.StartWorker(func(*gopcp_stream.StreamServer) *gopcp.Sandbox {
-		return gopcp.GetSandbox(map[string]*gopcp.BoxFunc{
-			"getServiceType": gopcp.ToSandboxFun(func(args []interface{}, attachment interface{}, pcpServer *gopcp.PcpServer) (interface{}, error) {
-				return "session_obrero", nil
-			}),
-
+	stdserv.StartStdWorker(&appConfig, func(naPools *napool.NAPools, s *gopcp_stream.StreamServer) map[string]*gopcp.BoxFunc {
+		return map[string]*gopcp.BoxFunc{
 			// (encryptSession, sessionText)
 			"encryptSession": gopcp.ToSandboxFun(mids.LogMid("encryptSession", func(args []interface{}, attachment interface{}, pcpServer *gopcp.PcpServer) (interface{}, error) {
 				// parse args
@@ -58,7 +45,7 @@ func main() {
 					text    string
 					timeout int
 				)
-				err := utils.ParseArgs(args, []interface{}{&text, &timeout}, "(getUidFromSessionText, sessionText)")
+				err := utils.ParseArgs(args, []interface{}{&text, &timeout}, "(getUidFromSessionText, sessionText, timeout)")
 				if err != nil {
 					return nil, err
 				}
@@ -73,13 +60,8 @@ func main() {
 				// query user service to get uid
 				return naPools.CallProxy(appConfig.AUTH_WP_NAME, pcpClient.Call(appConfig.AUTH_METHOD, sessionTxt), time.Duration(timeout)*time.Second)
 			})),
-		})
-	}, obrero.WorkerStartConf{
-		PoolSize:            2,
-		Duration:            20 * time.Second,
-		RetryDuration:       20 * time.Second,
-		NAGetClientMaxRetry: 3,
+		}
+	}, stdserv.StdWorkerConfig{
+		ServiceName: "session_obrero",
 	})
-
-	utils.RunForever()
 }
